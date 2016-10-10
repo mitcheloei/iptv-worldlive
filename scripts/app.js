d425c6ab0ac8893b0d7d5eef67bb6844b3f6715d
Michel <mitcheloei@users.noreply.github.com>
2016-10-10 17:13:35 +0200

    Add files via upload

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,19 @@
-// Define the `phonecatApp` module
+// Define the `ipvApp` module
 var ipvApp = angular.module('ipvApp', []);
+
+ipvApp.config(['$httpProvider', function($httpProvider) {
+        $httpProvider.defaults.useXDomain = true;
+        delete $httpProvider.defaults.headers.common['X-Requested-With'];
+    }
+]);
+
+// Added all header request and response.
+/*ipvApp.all('/*', function (request, response, next) {
+    response.header("Access-Control-Allow-Origin", "*");
+    response.header("Access-Control-Allow-Headers", "X-Requested-With");
+    response.header("Access-Control-Allow-Methods", "GET, POST", "PUT", "DELETE");
+    next();
+});*/
 
 // Define the `PhoneListController` controller on the `ipvApp` module
 ipvApp.controller('IptvController', function IptvController($scope, $http) {
@@ -7,11 +21,13 @@ ipvApp.controller('IptvController', function IptvController($scope, $http) {
     $scope.files =[];
     $http.get('../files/')
     .success(function(data) {
-         $(data).find("a:contains(.M3U)").each(function(){
+        $(data).find("a:contains(.M3U)").each(function(){
             // will loop through 
              var file ={};
+             file.id=this.text.split(".")[0];
              file.name =this.text
-             file.url =this.href
+             file.url = this.href
+             file.show = false;
              $http.get(file.url)
              .success(function(res) {
                 var lines = res.split("\n");
@@ -20,6 +36,10 @@ ipvApp.controller('IptvController', function IptvController($scope, $http) {
              });
         });
     });
+    
+    $scope.toggler = function(data) {
+        data.show = !data.show;
+    };
     
     function parseLines(lines) {
         var result = [];
@@ -31,11 +51,32 @@ ipvApp.controller('IptvController', function IptvController($scope, $http) {
                 var map = line.split(",");
                 data.name=map[1];
                 data.group=getGroup(map[0]);
-                data.status="OFF";
+                data.image = '../img/loading.gif';
+                setStatus(data,lines[i+1]);
                 result.push(data);
             }
         }
         return result;
+    };
+    
+    function setStatus(data,url) {
+        var result = "";
+        if(url){
+            $http.head('http://crossorigin.me/'+url).
+                success(function(res, status, headers, config) {
+                    if(status==200 &&headers()["content-type"]=="video/mp4"){
+                        data.status = "ON";
+                        data.image = '../img/ok.png';
+                    }else{
+                        data.status = "OFF";
+                        data.image = '../img/ko.png';
+                    }
+                })
+                .error(function(res, status, headers, config) {
+                    data.status = "OFF";
+                    data.image = '../img/ko.png';
+                });
+        }
     };
     
     function getGroup(text) {
@@ -43,8 +84,7 @@ ipvApp.controller('IptvController', function IptvController($scope, $http) {
         var map2 = text.split(" ");
         for (j = 0; j < map2.length; j++) { 
             if(map2[j].startsWith("group-title")){
-                console.info(map2[j]);
-                result = map2[j];
+                result = map2[j].split("\"",3)[1];
            }
         }
         return result;
