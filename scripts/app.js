950fece9fa12fe204f2c98cca36c4cedad89f27e
Michel <mitcheloei@users.noreply.github.com>
2016-10-13 16:50:36 +0200

    Add files via upload

--- scripts/app.js
+++ scripts/app.js
@@ -1,24 +1,15 @@
 // Define the `ipvApp` module
 var ipvApp = angular.module('ipvApp', []);
 
-ipvApp.config(['$httpProvider', function($httpProvider) {
-        $httpProvider.defaults.useXDomain = true;
-        delete $httpProvider.defaults.headers.common['X-Requested-With'];
-    }
-]);
-
-// Added all header request and response.
-/*ipvApp.all('/*', function (request, response, next) {
-    response.header("Access-Control-Allow-Origin", "*");
-    response.header("Access-Control-Allow-Headers", "X-Requested-With");
-    response.header("Access-Control-Allow-Methods", "GET, POST", "PUT", "DELETE");
-    next();
-});*/
-
 // Define the `PhoneListController` controller on the `ipvApp` module
 ipvApp.controller('IptvController', function IptvController($scope, $http) {
     
     $scope.files =[];
+    
+    $scope.toggler = function(data) {
+        data.show = !data.show;
+    };
+    
     $http.get('../files/')
     .success(function(data) {
         $(data).find("a:contains(.M3U)").each(function(){
@@ -28,20 +19,18 @@ ipvApp.controller('IptvController', function IptvController($scope, $http) {
              file.name =this.text
              file.url = this.href
              file.show = false;
+             file.numberOn=0;
+             file.numberOff=0;
              $http.get(file.url)
              .success(function(res) {
                 var lines = res.split("\n");
-                file.lines = parseLines(lines);
+                file.lines = parseLines(lines,file);
                 $scope.files.push(file);
              });
         });
     });
     
-    $scope.toggler = function(data) {
-        data.show = !data.show;
-    };
-    
-    function parseLines(lines) {
+    function parseLines(lines,file) {
         var result = [];
         for (i = 0; i < lines.length; i++) { 
             var line = lines[i];
@@ -51,61 +40,69 @@ ipvApp.controller('IptvController', function IptvController($scope, $http) {
                 var map = line.split(",");
                 data.name=map[1];
                 data.group=getGroup(map[0]);
-                data.image = '../img/loading.gif';
-                setStatus(data,lines[i+1]);
+                data.logo=getLogo(map[0]);
+                data.url=lines[++i];
+                data.statusimage = '../img/loading.gif';
+                setStatus(data,file);
                 result.push(data);
             }
         }
         return result;
     };
     
-    function setStatus(data,url) {
+    function setStatus(data,file) {
         var result = "";
+        var url = data.url;
         if(url){
             $http.head('http://crossorigin.me/'+url).
                 success(function(res, status, headers, config) {
-                    if(status==200 &&headers()["content-type"]=="video/mp4"){
-                        data.status = "ON";
-                        data.image = '../img/ok.png';
+                    if(status==200 && validateHeaderType(headers()["content-type"])){
+                        data.status = status;
+                        data.statusimage = '../img/ok.png';
+                        file.numberOn=file.numberOn+1;
                     }else{
-                        data.status = "OFF";
-                        data.image = '../img/ko.png';
+                        data.status = status;
+                        data.statusimage = '../img/ko.png';
+                        file.numberOff = file.numberOff+1;
                     }
                 })
                 .error(function(res, status, headers, config) {
-                    data.status = "OFF";
-                    data.image = '../img/ko.png';
+                    data.status = status;
+                    data.statusimage = '../img/ko.png';
+                    file.numberOff = file.numberOff+1;
                 });
         }
     };
     
+    function validateHeaderType(type){
+        var result=false;
+        var types = ["application/x-mpegurl",
+                     "application/vnd.apple.mpegurl",
+                     "video/mp4"
+                    ];
+        if(types.indexOf(type)>=0){
+            result = true;
+        }
+        return result;
+    }
+    
     function getGroup(text) {
         var result = "";
-        var map2 = text.split(" ");
-        for (j = 0; j < map2.length; j++) { 
-            if(map2[j].startsWith("group-title")){
-                result = map2[j].split("\"",3)[1];
-           }
+        var index = text.indexOf("group-title");
+        if(index>=0){
+            result = text.substr(index).split("\"",2)[1];
         }
         return result;
     };
     
-
-    
-    $scope.iptvs = [
-        {
-          name: 'Nexus S',
-          group: 'Fast just got faster with Nexus S.',
-          status: 'ON'
-        }, {
-          name: 'Motorola XOOM™ with Wi-Fi',
-          group: 'The Next, Next Generation tablet.',
-          status: 'ERROR'
-        }, {
-          name: 'MOTOROLA XOOM™',
-          group: 'The Next, Next Generation tablet.',
-          status: 'OFF'
+    function getLogo(text) {
+        var result = "";
+        var index = text.indexOf("tvg-logo");
+        if(index>=0){
+            result = text.substr(index).split("\"",2)[1];
         }
-    ];
+        console.info(result);
+        return result;
+    };
 
 });
